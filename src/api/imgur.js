4fce64e99cab9a12e4f63da4ff302bdde598d4ef
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-06-15 23:52:40 -0400

    added images module

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,21 +1,33 @@
+/* eslint-disable no-unused-vars */
 const ROOT_URL = 'http://localhost:8080';
 //TODO: uncomment below and comment above after making imgur accout
-/*
+//const ROOT_URL = 'https://api.imgur.com';
+
 import qs from 'qs';
+import axios from 'axios';
 const CLIENT_ID = 'clientIdChangeLater';
-const ROOT_URL = 'https://api.imgur.com';
-*/
 
 export default {
   login() {
-    window.location = `${ROOT_URL}/oauth2/callback#access_token=1234567890`;
-    //TODO: uncomment below and comment above after making imgur accout
-    /*
     const querystring = {
       client_id: CLIENT_ID,
       response_type: 'token',
     };
-    window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+
+    window.location = `${ROOT_URL}/oauth2/callback#access_token=1234567890`;
+    //TODO: uncomment below and comment above after making imgur accout
+    //window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+  },
+  fetchImages(token) {
+    //TODO: uncomment below and comment above after making imgur accout
+    /*
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     */
   },
 };
+
+/* eslint-disable no-unused-vars */
